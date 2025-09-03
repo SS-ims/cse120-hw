267a7a983d0b47392777a30eae86867b0abfd543
SS-ims <solosakala@gmail.com>
2025-09-03 15:32:09 +0200

    added some input and print statements for my motto

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.Write("Solomon in C# here I come");
+
+        Console.Write("Whats the moto? ");
+        string moto = Console.ReadLine();
+        Console.WriteLine($"Your moto is: {moto}");
     }
 }
