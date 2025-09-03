8fe12f7699d8c30f0e57380eef0b4166c0b5c9da
SS-ims <solosakala@gmail.com>
2025-09-03 16:24:51 +0200

    add user input for first name and display it

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name ");
+
+        string first = Console.ReadLine();
+
+        Console.WriteLine($"your first name is {first}");
     }
 }
