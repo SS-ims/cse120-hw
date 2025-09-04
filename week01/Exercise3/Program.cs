ae81d28ad486cdeb3d7a6c0254232040b8c0132b
SS-ims <solosakala@gmail.com>
2025-09-04 15:13:33 +0200

    refactor: restructure program to implement a magic number guessing game with guess counter

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string answer = "yes";
+
+        while (answer == "yes")
+        {
+            Random randomGenerator = new Random();
+            int randNumber = randomGenerator.Next(1, 10);
+            int check = 0;
+            int counter = 0;
+            do
+            {
+
+                Console.WriteLine("Enter the Magic Number:");
+                string guess = Console.ReadLine();
+                int magicNumber = int.Parse(guess);
+                check = magicNumber;
+
+                // increment counter
+                counter = counter + 1;
+
+
+                // prompt user to guess again with a higher number
+                if (magicNumber < randNumber)
+                {
+                    Console.WriteLine("Higher");
+
+                }
+                // prompt user to guess again with a lower number
+                else if (magicNumber > randNumber)
+                {
+                    Console.WriteLine("Lower");
+
+                }
+                // check if magic number is found
+                else if (magicNumber == randNumber)
+                {
+                    Console.WriteLine("You found the magic number!");
+                    Console.WriteLine($"It took you {counter} tries to find the magic number {randNumber}");
+                }
+                else
+                {
+                    Console.WriteLine("Just an ordinary number");
+                }
+            } while (check != randNumber);
+
+            // check if user wants to play again
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            answer = Console.ReadLine();
+        }
     }
 }
