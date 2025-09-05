9b2be65d8b18222f3648df34fdd70db64aa27a6a
SS-ims <solosakala@gmail.com>
2025-09-05 11:32:57 +0200

    feat: add methods for user interaction and number squaring functionality

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,69 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void DisplayWelcome()
+{
+    Console.WriteLine("Welcome to the Program!");
+}
+
+public static string PromptUserName()
+{
+    Console.Write("Please enter your name: ");
+    return Console.ReadLine();
+}
+
+public static int PromptUserNumber()
+{
+    Console.Write("Please enter your favorite number: ");
+    return int.Parse(Console.ReadLine());
+}
+
+public static int SquareNumber(int number)
+{
+    return number * number;
+}
+
+public static void DisplayResult(string name, int squaredNumber)
+{
+    Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+}
+
+public class Assignment
+{
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
     }
+
+    public static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    public static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    public static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    public static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+    }
+}
 }
